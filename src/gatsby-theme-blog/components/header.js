b62b3dd5b2116d5945184d1706e618261a12a17e
shikun <kunish.butt@gmail.com>
2021-06-11 21:57:05 +0800

    feat: add the ability to go back home page

--- src/gatsby-theme-blog/components/header.js
+++ src/gatsby-theme-blog/components/header.js
@@ -1,3 +1,34 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { NavLink, Box } from 'theme-ui'
+import GithubCorner from 'react-github-corner'
+
+const HomeLink = (props) => {
+  return (
+    <Link
+      activeStyle={{
+        color: 'black',
+      }}
+      style={{
+        width: '100%',
+        lineHeight: '80px',
+        color: 'black',
+        textAlign: 'center',
+      }}
+      {...props}
+    >
+      Kunish
+    </Link>
+  )
+}
+
 export default function Header() {
-  return null
+  return (
+    <>
+      <Box bg="primary">
+        <NavLink as={HomeLink} to="/" opacity={1} color="black" />
+      </Box>
+      <GithubCorner href="https://github.com/kunish/blog" />
+    </>
+  )
 }
